230e6f5d151411b2fe2fcfd7111c1eacda64f702
Justin Winter <justinlevi@gmail.com>
2018-05-15 13:37:07 -0400

    Initial Commit 🤩

---
+++ README.md
@@ -0,0 +1,15 @@
+##Main Resources
+
+https://medium.freecodecamp.org/building-an-electron-application-with-create-react-app-97945861647c
+
+From React to an Electron app ready for production
+This was ultimately the resource that got things working for me
+https://medium.com/@kitze/%EF%B8%8F-from-react-to-an-electron-app-ready-for-production-a0468ecb1da3
+
+Here’s the github repo
+https://github.com/kitze/react-electron-example
+
+Here is the PR that actually finally got everything working:
+https://github.com/kitze/react-electron-example/pull/3/files
+
+$ `yarn electron-dev`
---
+++ package/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const remote = window.require('electron').remote;
+const appVersion = remote.app.getVersion();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
+      </div>
+      <p className="App-intro">
+       Version: {appVersion}
+      </p>
+    </div>
+    );
+  }
+}
+
+export default App;
